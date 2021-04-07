011712f75f0d175978e054bfe89ffe5f37c70f74
internalinterference <kanequbolt@gmail.com>
2021-04-07 13:40:20 +1000

    first

---
+++ Watch/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watch.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string userName, string roomCode)
+        {
+            string data = $"Username: {userName}, Roomcode: {roomCode}";
+            return View(data);
+        }
+    }
+}
