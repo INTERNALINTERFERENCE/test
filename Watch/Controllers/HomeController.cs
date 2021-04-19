e0609e3a7fd1b54a219436daf5f407bd7cf6e1e3
internalinterference <kanequbolt@gmail.com>
2021-04-19 14:19:25 +1000

    add videoplayer view

--- Watch/Controllers/HomeController.cs
+++ Watch/Controllers/HomeController.cs
@@ -10,32 +10,10 @@ namespace Watch.Controllers
 {
     public class HomeController : Controller
     {
-        static RoomData roomData = new RoomData();
         [HttpGet]
         public IActionResult Index()
         {
             return View();
-        }
-
-        //join room button
-        public RedirectResult JoinRoom(string userName, string roomCode)
-        {
-            //only for test
-            //roomData.UserName = userName;
-            //roomData.RoomCode = roomCode;
-            //return Content($"{userName} joined the room");
-            return Redirect("Player/VideoPlayer");
-        }
-
-        //create room button
-        public IActionResult CreateRoom(string userName)
-        {
-            //only for test
-            roomData.Users.Add(userName);
-            var users = roomData.Users;
-
-            return Content($"{userName} created the room, users online: {String.Join(',', users)}");
-        }
-      
+        }   
     }
 }
---
+++ Watch/Controllers/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Watch.Models;
+
+namespace Watch.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public static RoomData roomData = new RoomData();
+
+        public IActionResult VideoPlayer(string userName, string roomCode)
+        {
+            roomData.UserName = userName;
+            roomData.RoomCode = roomCode;
+
+            return View(roomData);
+        }
+    }
+}
